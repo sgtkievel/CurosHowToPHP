9643a0d4da7025cf11c0edfa3596ee11df64e14c
Leandro Kievel <jlk-sapoverde@hotmail.com>
2016-09-03 17:15:19 -0300

    iniciando php

---
+++ aula1/iniciando.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php
+    $var = 'Leandro';
+?>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?= $var; ?>
+    </body>
+</html>
